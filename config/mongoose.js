5d009ec92876ed9ae63f3a957ab066f758c4619a
Manish Yadav <129028806+Manishyadavkanpur@users.noreply.github.com>
2023-06-29 09:29:58 +0530

    first commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,35 +1,35 @@
-// const mongoose = require('mongoose');
-// // mongoose.connect('mongodb://0.0.0.0:27017/placement');
-// const DB = 'mongodb+srv://birajjyo2:uhnekDGnu6zglcFo@cluster0.kybvwzo.mongodb.net/test';
+const mongoose = require('mongoose');
+//mongoose.connect('mongodb://0.0.0.0:27017/placement');
+ const DB = 'mongodb+srv://yadavv7880:1SsOdLXnB6rwIUr4@cluster0.delxxlc.mongodb.net/placement_cell';
 
-// mongoose.connect(DB, {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-// // error
-// db.on('error',console.error.bind(console,'erroe connecting to db'));
-// // up and running then message
-// db.once('open',function(){
-//     console.log('Success fully connected to the database')
-// })
-
-
-
-
-
-
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/placement_cell');
-//mongoose.connect("mongodb://localhost:27017/codeial", { useNewUrlParser: true });
-
-const db=mongoose.connection;
-
-db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
-
-db.once('open',function(){
-    console.log('Connected to Database :: MongoDB');
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
-module.exports=db;
+const db = mongoose.connection;
+// error
+db.on('error',console.error.bind(console,'erroe connecting to db'));
+// up and running then message
+db.once('open',function(){
+    console.log('Success fully connected to the database')
+})
+
+
+
+
+
+
+// const mongoose=require('mongoose');
+// mongoose.connect('mongodb://0.0.0.0:27017/placement_cell');
+// //mongoose.connect("mongodb://localhost:27017/codeial", { useNewUrlParser: true });
+
+// const db=mongoose.connection;
+
+// db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
+
+// db.once('open',function(){
+//     console.log('Connected to Database :: MongoDB');
+// });
+
+// module.exports=db;
